6336ae9b7cfbdf3a134fc550ed6897d61c67b088
astizm <asti-maera@live.nmit.ac.nz>
2021-04-07 09:59:49 +1200

    week six functions

---
+++ src/weekSix.js
@@ -0,0 +1,31 @@
+//
+function firstFunction() {
+    console.log("Hello from firstFunction");
+    thirdFunction(); //called after output from first
+}
+function secondFunction() {
+    firstFunction(); //first so called first and executed before output from second
+    console.log("The end from secondFunction");
+}
+function thirdFunction() {
+    console.log(`what's up from the middle`);
+};
+secondFunction(); //Call second to call first
+
+myArray = [];
+myArray.push('first');
+myArray.push('second');
+myArray.push('third');
+myArray.push('fourth');
+myArray.shift();
+//myArray//['second', 'third', 'fourth']
+
+function bottleCapper(a, b) {
+    return a + b;
+}
+
+console.log(bottleCapper(bottleCapper('green bottle', 'white cap')));
+
+function sum(a, b, c, d) {
+    return a + b + c + d;
+};
